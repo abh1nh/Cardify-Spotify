625d2f480bf920a1b2fa3c209da01d79a5564483
Abhiram Neelamraju <abhiramnh.2002@gmail.com>
2024-10-08 13:47:38 -0400

    Frontend & Backend setup

---
+++ cardify-frontend/src/Home.js
@@ -0,0 +1,10 @@
+const Home = () => {
+    return ( 
+        <div className="Home">
+            <h1>App component</h1>
+        </div>
+
+    );
+}
+ 
+export default Home;
---
+++ cardify-frontend/src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>Cardify</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/about">About</a>
+                <a href="/privacypolicy">Privacy Policy</a>
+            </div>
+        </nav> );
+}
+ 
+export default Navbar;
